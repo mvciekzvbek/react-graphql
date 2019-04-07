3dbd6bc502dd102be42efd750939d060aaf48bff
Maciej Ząbek <maciek@maciek.local>
2019-04-07 14:57:10 +0200

    mutatuon, refetch query

--- src/Users.js
+++ src/Users.js
@@ -1,31 +1,55 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import { gql } from 'apollo-boost';
+
+const ROOT_QUERY = gql`
+    {   
+        totalUsers
+        allUsers {
+            githubLogin
+            name
+            avatar
+        }
+    }
+`
 
 const Users = () => 
     <Query 
-        query={gql`
-            {
-                totalUsers
-                allUsers {
-                    githubLogin
-                    name
-                    avatar
-                }
-            }
-        `}
+        query={ROOT_QUERY}
+        // pollInterval={60000}
     >
-        {({ data, error, loading }) => {
+        {({ data, error, loading, refetch }) => {
             if (loading) return <p>Loading...</p>
             if (error) return <p>Error</p>
-            return <UserList count={data.totalUsers} users={data.allUsers}/>
+            return <UserList 
+                count={data.totalUsers} 
+                users={data.allUsers} 
+                refetch={refetch} />
         }}
     </Query>
 
-const UserList = ({count, users}) => {
+const UserList = ({count, users, refetch}) => {
     return (
         <div>
             <p>{count} users</p>
+            <button onClick={() => refetch()}>Refetch</button>
+            <Mutation 
+                mutation={gql`
+                    mutation addFakeUsers($count: Int!) {
+                        addFakeUsers(count: $count) {
+                            githubLogin,
+                            name,
+                            avatar
+                        }
+                }
+            `}
+            variables={{count: 1}} 
+            refetchQueries={[{query: ROOT_QUERY}]}
+            >
+                {addFakeUsers =>
+                    <button onClick={addFakeUsers}>Add Fake Users</button>
+                }
+            </Mutation>
             <ul>
                 {users.map(user => 
                     <UserListItem
